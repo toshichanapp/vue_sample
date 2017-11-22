3eb5e18c5ddcc427f529b2a75ebeca4ec0fdbfba
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-22 13:51:48 +0900

    chapter4

--- shopping-list/src/App.vue
+++ shopping-list/src/App.vue
@@ -1,30 +1,47 @@
 <template>
   <div id="app" class="container">
-    <h2>{{ title }}</h2>
-    <add-item-component></add-item-component>
-    <items-component :items="items"></items-component>
-    <div class="footer">
-      <hr/>
-      <change-title-component></change-title-component>
+    <ul class="nav nav-tabs" role="tablist">
+      <li :class= "index===0 ? 'active' : ''" v-for="(list, index) in shoppinglists" role="presentation">
+        <a :href="'#' + list.id" :aria-controls="list.id" role="tab" data-toggle="tab">{{ list.title }}</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div :class= "index===0 ? 'active' : ''" v-for="(list, index) in shoppinglists" class="tab-pane" role="tabpanel" :id="list.id">
+        <shopping-list-component :title="list.title" :items="list.items"></shopping-list-component>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import AddItemComponent from './components/AddItemComponent'
-  import ItemsComponent from './components/ItemsComponent'
-  import ChangeTitleComponent from './components/ChangeTitleComponent'
+  import ShoppingListComponent from './components/ShoppingListComponent'
 
   export default {
     components: {
-      AddItemComponent,
-      ItemsComponent,
-      ChangeTitleComponent
+      ShoppingListComponent
     },
     data () {
       return {
-        items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }]
+        shoppinglists: [
+          {
+            id: 'groceries',
+            title: 'Groceries',
+            items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }]
+          },
+          {
+            id: 'clothes',
+            title: 'Clothes',
+            items: [{ text: 'black dress', checked: false }, { text: 'all stars', checked: false }]
+          }
+        ]
       }
     }
   }
 </script>
+
+<style>
+  .container {
+    width: 40%;
+    margin: 20px auto 0px auto;
+  }
+</style>
--- shopping-list/src/components/ChangeTitleComponent.vue
+++ shopping-list/src/components/ChangeTitleComponent.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    change title
+    <em>Change the title of your shopping list here</em>
+    <input :value="value" @input="onInput"/>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['value'],
+    methods: {
+      onInput (event) {
+        this.$emit('input', event.target.value)
+      }
+    }
+  }
 </script>
 
 <style scoped>
