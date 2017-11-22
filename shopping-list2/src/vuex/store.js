2252f5a2dc2b6e01a0591e6461f9911614fbf28b
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-22 10:08:25 +0900

    mutation actionsへ書き換え

--- shopping-list2/src/vuex/store.js
+++ shopping-list2/src/vuex/store.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 import actions from './actions'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 const state = {
@@ -20,5 +21,5 @@ const state = {
 }
 
 export default new Vuex.Store({
-  state, getters, actions
+  state, mutations, getters, actions
 })
