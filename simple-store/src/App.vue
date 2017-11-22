cf33bc20eb7d793d8d9c61a3c7db9355a33d21ad
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-22 16:07:59 +0900

    create new simple store

---
+++ simple-store/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <show-greetings-component :msg='msg'></show-greetings-component>
+    <change-greetings-component :msg='msg'></change-greetings-component>
+  </div>
+</template>
+
+<script>
+  import ShowGreetingsComponent from './components/ShowGreetingsComponent.vue'
+  import ChangeGreetingsComponent from './components/ChangeGreetingsComponent.vue'
+  export default {
+    components: {ShowGreetingsComponent, ChangeGreetingsComponent},
+    data() {
+      return {
+        msg: 'App vue'
+      }
+    }
+  }
+</script>
