118392a6d068b70eb98456b97548029eff281e99
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-22 16:31:26 +0900

    getterの利用

--- simple-store/src/vuex/store.js
+++ simple-store/src/vuex/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
 
 Vue.use(Vuex)
 
@@ -14,5 +15,5 @@ const mutations = {
 }
 
 export default new Vuex.Store({
-  state, mutations
+  state, mutations, getters
 })
