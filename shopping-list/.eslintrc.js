803f7bc3ffeed3581dcf5055a470fc28902b82db
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-22 13:48:19 +0900

    chapter3

--- shopping-list/.eslintrc.js
+++ shopping-list/.eslintrc.js
@@ -1,15 +1,10 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
--- shopping-list/src/App.vue
+++ shopping-list/src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="container">
+    <h2>{{ title }}</h2>
+    <add-item-component></add-item-component>
+    <items-component :items="items"></items-component>
+    <div class="footer">
+      <hr/>
+      <change-title-component></change-title-component>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
+  import AddItemComponent from './components/AddItemComponent'
+  import ItemsComponent from './components/ItemsComponent'
+  import ChangeTitleComponent from './components/ChangeTitleComponent'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  export default {
+    components: {
+      AddItemComponent,
+      ItemsComponent,
+      ChangeTitleComponent
+    },
+    data () {
+      return {
+        items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }]
+      }
+    }
+  }
+</script>
