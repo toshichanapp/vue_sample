99b14f1a1ea60169cfda1e78ddfd9e34952cb5c3
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-24 14:05:34 +0900

    1st

---
+++ filter-sample/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import store from './vuex/store'
+import { mapGetters } from 'vuex'
+
+export default {
+  store,
+  computed: mapGetters({
+    
+  })
+}
+</script>
+
+<style>
+</style>
