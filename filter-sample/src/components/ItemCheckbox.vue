de46d1c213d5b6258688c53853b9edc7986dfa97
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-27 09:54:42 +0900

    filter機能追加

--- filter-sample/src/components/ItemCheckbox.vue
+++ filter-sample/src/components/ItemCheckbox.vue
@@ -6,19 +6,29 @@
       .modal-body
         .mb-3
           .input-group
-            input.form-control(type="search", placeholder="Search...", @input="onInput({ name: $event.target.value })")
-        .form-checkbox.mb-0(v-for="item in items", :key="item.id")
+            input.form-control(type="search", placeholder="Search...", v-model="query")
+        .form-checkbox.mb-0(v-for="item in filteredItems", :key="item.id")
           label.custom-control.custom-checkbox
             input.custom-control-input(type="checkbox", :value="item.id", name="group[item_ids][]")
             span.custom-control-indicator
             span.custom-control-description {{item.name}}
 </template>
 <script>
-  import { mapActions } from 'vuex'
+  import { mapState } from 'vuex'
   export default{
-    props: ['items', 'name'],
-    methods: mapActions({
-      onInput: 'filterItems'
-    })
+    props: ['name'],
+    data() {
+      return {
+          query: ""
+      }
+    },
+    computed: {
+      ...mapState(['items']),
+      filteredItems: function() {
+        return this.items.filter(function(item) {
+          return item.name.indexOf(this.query) > -1
+        }, this)
+      }
+    }
   }
 </script>
