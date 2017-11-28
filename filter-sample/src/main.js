d5810e41724545ed76138aff1a44d19db088cc41
Toshiki Fujiyoshi <toshichan@synapz.jp>
2017-11-28 16:59:33 +0900

    データをvm作成時に登録するように変更

--- filter-sample/src/main.js
+++ filter-sample/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './vuex/store'
 
 new Vue({
   el: '#app',
+  created: function() {
+    store.state.items = [{"id":1,"name":"synapz","size":40},
+                        {"id":51,"name":"鷹の爪団","size":0},
+                        {"id":52,"name":"秘密結社","size":2},
+                        {"id":53,"name":"宇宙忍者","size":1},
+                        {"id":54,"name":"富士急行","size":1},
+                        {"id":55,"name":"オリジン電気","size":0}],
+    store.state.columns = ['名前','メンバ']
+  },
   render: h => h(App)
 })
--- filter-sample/src/vuex/store.js
+++ filter-sample/src/vuex/store.js
@@ -7,14 +7,9 @@ import mutations from './mutations'
 Vue.use(Vuex)
 
 const state = {
-  items: [{"id":1,"name":"synapz","size":40},
-          {"id":51,"name":"鷹の爪団","size":0},
-          {"id":52,"name":"秘密結社","size":2},
-          {"id":53,"name":"宇宙忍者","size":1},
-          {"id":54,"name":"富士急行","size":1},
-          {"id":55,"name":"オリジン電気","size":0}],
+  items: [],
   currentChecked: [],
-  columns: ['名前','メンバ']
+  columns: []
 }
 
 export default new Vuex.Store({
